4eeb36b83e827b3f2b29243b38e46b3563913aaf
ASZIIIS <asziiis@163.com>
2023-07-08 20:12:48 +0800

    commit

--- ciga_gamejam_2023/Assets/Sciptes/Controller/PlayerController.cs
+++ ciga_gamejam_2023/Assets/Sciptes/Controller/PlayerController.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    protected GameObject currentOneWayPlayform;
-    [SerializeField] protected CapsuleCollider2D palyerCollider;
-    protected BoxCollider2D playerController;
-    protected Rigidbody2D rb2D;
+    private GameObject currentOneWayPlayform;
+    [SerializeField] private CapsuleCollider2D palyerCollider;
+    private BoxCollider2D playerController;
+    private Rigidbody2D rb2D;
     Animator animator;
-    [SerializeField] protected float moveSpeed;
-    [SerializeField] protected float jumpForce;
-    protected bool _isJumping = false;
-    protected bool _isFalling = false;
-    protected bool _isRunning = false;
-    protected bool isFacingRight = true;
-    protected bool _isStunned = false;
-    protected Skill currentSkill;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    private bool _isJumping = false;
+    private bool _isFalling = false;
+    private bool _isRunning = false;
+    private bool isFacingRight = true;
+    private bool _isStunned = false;
+    private Skill currentSkill;
+    [SerializeField] private bool character;
 
-    protected bool isFalling
+    private bool isFalling
     {
         get
         {
@@ -38,27 +39,27 @@ public class PlayerController : MonoBehaviour
         {
             return _isJumping;
         }
-        protected set
+        private set
         {
             _isJumping = value;
             animator.SetBool("isJumping", value);
         }
     }
-    protected float moveHorizontal;
-    protected float moveVertical;
+    private float moveHorizontal;
+    private float moveVertical;
     public bool isRunning
     {
         get
         {
             return _isRunning;
         }
-        protected set
+        private set
         {
             _isRunning = value;
             animator.SetBool("isRunning", value);
         }
     }
-    protected void Awake()
+    private void Awake()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -66,10 +67,52 @@ public class PlayerController : MonoBehaviour
     }
     void Start()
     {
-
+        //character=false;
 
     }
-    protected void FixedUpdate()
+    void Update()
+    {
+        if(character){
+            moveHorizontal = Input.GetAxisRaw("Horizontal1");
+            moveVertical = Input.GetAxisRaw("Vertical1");
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (!_isStunned && currentOneWayPlayform != null && currentOneWayPlayform.tag != "Platform")
+                {
+                    StartCoroutine(DisableCollision());
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!_isStunned && currentSkill != null)
+                {
+                    currentSkill.UseSkill();
+                    currentSkill=null;
+                }
+            }
+        }else{
+            moveHorizontal = Input.GetAxisRaw("Horizontal2");
+            moveVertical = Input.GetAxisRaw("Vertical2");
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (!_isStunned&&currentOneWayPlayform != null && currentOneWayPlayform.tag != "Platform")
+                {
+                    StartCoroutine(DisableCollision());
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightControl))
+            {
+                if (!_isStunned && currentSkill != null)
+                {
+                    currentSkill.UseSkill();
+                    currentSkill=null;
+                }
+            }
+        }
+        
+
+    }
+    private void FixedUpdate()
     {
         if (!_isStunned) {
             if (moveHorizontal > 0.1f || moveHorizontal < -0.1f)
@@ -95,7 +138,7 @@ public class PlayerController : MonoBehaviour
         
 
     }
-    protected GameObject OnCollisionEnter2D(Collision2D collision)
+    private GameObject OnCollisionEnter2D(Collision2D collision)
 
     {
         if ((collision.gameObject.tag == "OneWayPlatform" || collision.gameObject.tag == "Platform") && gameObject.transform.position.y - collision.gameObject.transform.position.y > 0 && rb2D.velocity.y == 0)
@@ -106,7 +149,7 @@ public class PlayerController : MonoBehaviour
         return collision.gameObject;
     }
 
-    protected void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "OneWayPlatform" || collision.gameObject.tag == "Platform")
         {
@@ -115,7 +158,7 @@ public class PlayerController : MonoBehaviour
 
         }
     }
-    protected void onFalling()
+    private void onFalling()
     {
         if (rb2D.velocity.y < 0f)
         {
@@ -129,14 +172,14 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    protected IEnumerator DisableCollision()
+    private IEnumerator DisableCollision()
     {
         BoxCollider2D platformCollider = currentOneWayPlayform.GetComponent<BoxCollider2D>();
         Physics2D.IgnoreCollision(palyerCollider, platformCollider);
         yield return new WaitForSeconds(0.5f);
         Physics2D.IgnoreCollision(palyerCollider, platformCollider, false);
     }
-    protected void FlipFace()
+    private void FlipFace()
     {
         if (isFacingRight && moveHorizontal < 0f || !isFacingRight && moveHorizontal > 0f)
         {
@@ -158,5 +201,8 @@ public class PlayerController : MonoBehaviour
     public void SetSkill(Skill skill)
     {
         currentSkill = skill;
+    }
+    public void SetCharacter(bool c){
+        character=c;
     }
 }
