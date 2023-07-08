7cf18ed5cf8ed30ed96b9c851576d9defd8cc0ff
ASZIIIS <asziiis@163.com>
2023-07-08 10:30:55 +0800

    test elevator

---
+++ ciga_gamejam_2023/Assets/Sciptes/Controller/ElevatorController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    public GameObject 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other){
+        
+    }
+ 
+    private void OnTriggerExit(Collider other){
+        
+    }
+
+}
